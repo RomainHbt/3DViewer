427adbf9e7116d99ad3eb7540c2db3151953ad8c
RomainCgr <chigar.romain@gmail.com>
2014-12-03 19:23:48 +0100

    Revert "Implémentation Bibliothèque"
    This reverts commit 760bd4b1db27b8f98138c455eb6f92e7b77fc943.

--- 3DViewer/src/exceptions/IncorrectFileException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-@SuppressWarnings("serial")
-public class IncorrectFileException extends Exception{
-
-	public IncorrectFileException(String message){
-		super(message);
-	}
-}
